0cf9f9ed1586a8d3ee6b78151847b2f5eb39b7b6
“ <1165291378@qq.com>
2019-03-20 13:40:53 +0800

    template_page v1.0

--- background/src/main/webapp/js/template_page.js
+++ background/src/main/webapp/js/template_page.js
@@ -5,7 +5,7 @@ var insertNodeBefore = function (newNode, oldNode){
 var leftNavBarDivWrap = document.createElement("div");
 var leftNavBarDiv = document.createElement("div");
 leftNavBarDiv.innerHTML = '' +
-    '    <ul class="nav nav-pills nav-stacked">\n' +
+    '    <ul class="nav nav-pills nav-stacked white-bg">\n' +
     '        <li class="active"><a href="#">首页</a></li>\n' +
     '        <li><a href="#">用户管理</a></li>\n' +
     '        <li>\n' +
@@ -54,29 +54,27 @@ insertNodeBefore(leftNavBarDivWrap, document.getElementById("mainBody"));
 leftNavBarDivWrap.appendChild(leftNavBarDiv);
 
 var topNavBarDiv = document.createElement("div");
-topNavBarDiv.innerHTML = '<nav class="navbar navbar-expand-lg navbar-light bg-light">\n' +
+topNavBarDiv.innerHTML = '<nav class="navbar navbar-expand-lg white-bg">\n' +
     '        <a class="navbar-brand" href="#">Pizza Express</a>\n' +
-    '    <div class="dropdown mr-sm-2">\n' +
-    '        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">\n' +
-    '            <div class="navbar-nav">\n' +
-    '                <div class="nav-item">\n' +
+    '    <div class="dropdown column pull-right">\n' +
+    '\n' +
+    '        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" data-toggle="dropdown" role="button"  aria-haspopup="true" aria-expanded="false">\n' +
+    '            <div class="container-fluid">\n' +
+    '                <div class="column">\n' +
     '                    <span class="glyphicon glyphicon-user"></span>\n' +
-    '                </div>\n' +
-    '                <div class="nav-item">\n' +
     '                    <div id="AdminName">AdminName</div>\n' +
-    '                </div>\n' +
-    '                <div class="nav-item">\n' +
     '                    <span class="glyphicon glyphicon-triangle-bottom"></span>\n' +
     '                </div>\n' +
     '            </div>\n' +
     '        </a>\n' +
-    '        <div class="dropdown-menu" aria-labelledby="navbarDropdown">\n' +
-    '            <a class="dropdown-item" href="#">item1</a>\n' +
-    '            <a class="dropdown-item" href="#">Another action</a>\n' +
-    '            <div class="dropdown-divider"></div>\n' +
-    '            <a class="dropdown-item" href="#">Something else here</a>\n' +
-    '        </div>\n' +
+    '        <ul class="dropdown-menu">\n' +
+    '            <li><a href="#">User Info</a></li>\n' +
+    '            <li><a href="#">Setting</a></li>\n' +
+    '            <li><a href="#">Something else here</a></li>\n' +
+    '            <li role="separator" class="divider"></li>\n' +
+    '            <li><a href="#">Sign Out</a></li>\n' +
+    '        </ul>\n' +
     '    </div>\n' +
-    '    </nav>';
+    '</nav>';
 topNavBarDiv.style.textAlign="center";
 insertNodeBefore(topNavBarDiv, document.body.firstElementChild);
