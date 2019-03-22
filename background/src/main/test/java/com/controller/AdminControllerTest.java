0d16a8b1703f67c41e1e6ee305dd831cdfbbe184
“ <1165291378@qq.com>
2019-03-22 08:25:13 +0800

    background - unit test template

---
+++ background/src/main/test/java/com/controller/AdminControllerTest.java
@@ -0,0 +1,22 @@
+package com.controller;
+
+import baseUnitTest.BaseUnitTest;
+import com.pojo.Admin;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.Assert.*;
+
+public class AdminControllerTest extends BaseUnitTest {
+    @Autowired
+    AdminController controller;
+    @Test
+    public void checkLogin() {
+        Admin admin =new Admin();
+        admin.setUsername("admin");
+        admin.setPassword("123456");
+        String ret = controller.checkLogin(admin);
+        Assert.assertEquals("success",ret);
+    }
+}
