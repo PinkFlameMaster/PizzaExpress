eb8fa45e78807a6c3ec47ca58883359eb3d530cc
bjg <baojiageng@yeah.net>
2019-03-27 15:40:31 +0800

    Factory 前端

--- background/src/main/webapp/js/Factory.js
+++ background/src/main/webapp/js/Factory.js
@@ -3,49 +3,34 @@ $(function () {
     //1.初始化Table
     var oTable = new TableInit();
     oTable.Init();
-    var orderId=getUrlParameter("id");
-    alert(orderId);
 
 
 });
-var getUrlParameter = function getUrlParameter(sParam) {
-    var sPageURL = window.location.search.substring(1),
-        sURLVariables = sPageURL.split('&'),
-        sParameterName,
-        i;
 
-    for (i = 0; i < sURLVariables.length; i++) {
-        sParameterName = sURLVariables[i].split('=');
-
-        if (sParameterName[0] === sParam) {
-            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
-        }
-    }
-};
 
 var TableInit = function () {
     var oTableInit = new Object();
     //初始化Table
     oTableInit.Init = function () {
-        $('#tb_item').bootstrapTable({
+        $('#tb_factory').bootstrapTable({
             url: '/Home/GetDepartment',         //请求后台的URL（*）
             method: 'get',                      //请求方式（*）
             clickEdit: false,                    //点击修改
-            //toolbar: '#toolbar',                //工具按钮用哪个容器
+            toolbar: '#toolbar',                //工具按钮用哪个容器
             striped: true,                      //是否显示行间隔色
             cache: false,                       //是否使用缓存，默认为true，所以一般情况下需要设置一下这个属性（*）
-            //pagination: true,                   //是否显示分页（*）
+            pagination: true,                   //是否显示分页（*）
             sortable: false,                     //是否启用排序
             sortOrder: "asc",                   //排序方式
             queryParams: oTableInit.queryParams,//传递参数（*）
-            //sidePagination: "server",           //分页方式：client客户端分页，server服务端分页（*）
-            //pageNumber:1,                       //初始化加载第一页，默认第一页
-            //pageSize: 10,                       //每页的记录行数（*）
-            //pageList: [10, 25, 50, 100],        //可供选择的每页的行数（*）
-            //search: true,                       //是否显示表格搜索，此搜索是客户端搜索，不会进服务端，所以，个人感觉意义不大
+            sidePagination: "client",           //分页方式：client客户端分页，server服务端分页（*）
+            pageNumber:1,                       //初始化加载第一页，默认第一页
+            pageSize: 10,                       //每页的记录行数（*）
+            pageList: [10, 25, 50, 100],        //可供选择的每页的行数（*）
+            search: false,                       //是否显示表格搜索，此搜索是客户端搜索，不会进服务端，所以，个人感觉意义不大
             strictSearch: true,
             showColumns: false,                  //是否显示所有的列
-            //showRefresh: true,                  //是否显示刷新按钮
+            showRefresh: true,                  //是否显示刷新按钮
             minimumCountColumns: 2,             //最少允许的列数
             clickToSelect: false,                //是否启用点击选中行
             uniqueId: "ID",                     //每一行的唯一标识，一般为主键列
@@ -55,35 +40,49 @@ var TableInit = function () {
             editable:false,
 
             columns: [ {
-                field: 'id',
-                title: '菜品编号',
-            }, {
                 field: 'name',
-                title: '菜品名称',
+                title: '门店名称',
+            }, {
+                field: 'address',
+                title: '门店地址',
                 edit:false,
             }, {
-                field: 'price',
-                title: '单价',
-                edit:false,
+                field: 'manager',
+                title: '联系人',
             }, {
-                field: 'amount',
-                title: '数量'
-            }, ],
+                field: 'phoneNum',
+                title: '联系方式',
+                edit:true,
+            },{
+                field: 'businessTimeFrom',
+                title: '营业起始时间',
+                edit:true,
+            },{
+                field: 'businessTimeTo',
+                title: '营业结束时间',
+                edit:true,
+            },
+            ],
 
             onClickCell: function(field, value, row, $element) {
-                if (field=="name")
-                {
-                    RequestItemDetail(row.id)
-                }
+                $element.attr('contenteditable', true);
+                $element.blur(function() {
+                    let index = $element.parent().data('index');
+                    let tdValue = $element.html();
+
+                    saveData(index, field, tdValue);
+                })
             }
         });
-        function  RequestItemDetail(id) {
-            alert(id);
+
+        function saveData(index, field, value) {
+            $table.bootstrapTable('updateCell', {
+                index: index,       //行索引
+                field: field,       //列名
+                value: value        //cell值
+            })
         }
-
-
     };
-    
 
     //得到查询的参数
     oTableInit.queryParams = function (params) {
@@ -108,27 +107,11 @@ var TableInit = function () {
 var mockData = [
     {
         "id": 0,
+        "businessTimeFrom":"22:00",
+        "businessTimeTo":"9:00",
         "name": "Item 0",
-        "price": "$0",
-        "amount":"abc"
-    },
-    {
-        "id": 1,
-        "name": "Item 1",
-        "price": "$1",
-        "amount":"abc"
-    },
-    {
-        "id": 2,
-        "name": "Item 2",
-        "price": "$2",
-        "amount":"abc"
-    },
-    {
-        "id": 3,
-        "name": "Item 3",
-        "price": "$3",
-        "amount":"abc"
+        "phoneNum": "p100019",
+        "manager":"abc"
     }
 ]
-$('#tb_item').bootstrapTable('load',mockData);   //这行代码在浏览器debug的console里输入，就有数据了。
+$('#tb_factory').bootstrapTable('load',mockData);   //这行代码在浏览器debug的console里输入，就有数据了。
