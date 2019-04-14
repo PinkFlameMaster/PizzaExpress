d8bc84a99109a619e96a2a9832668b8c1a8365b3
ptrcxy <caoxuyuan@outlook.com>
2019-04-14 14:12:19 +0800

    店面管理

--- background/src/main/java/com/service/FactoryService.java
+++ background/src/main/java/com/service/FactoryService.java
@@ -2,6 +2,12 @@ package com.service;
 
 import com.pojo.Factory;
 
+import java.util.List;
+
 public interface FactoryService {
-    Factory getFactory(int id);
+    List<Factory> getFactory(String name);
+    Factory getAdminFactory(int id);
+    void updateFac(Factory factory);
+    void createFac(Factory factory);
+    void deleteFac(int id);
 }
