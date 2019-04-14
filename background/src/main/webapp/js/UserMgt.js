2c3a9198364f0a5bb415edbf0f4c87505a5548a3
“ <1165291378@qq.com>
2019-04-14 14:41:06 +0800

    fix bug

--- background/src/main/webapp/js/UserMgt.js
+++ background/src/main/webapp/js/UserMgt.js
@@ -93,7 +93,7 @@ var TableInit = function () {
     //     var temp = {   //这里的键的名字和控制器的变量名必须一直，这边改动，控制器也需要改成一样的
     //     };
     //     return temp;
-    // };
+    };
     return oTableInit;
 
 };
